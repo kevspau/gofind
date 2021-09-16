ec2b5646a364dff9a80909262ef64e1a68d3e6e0
csharpdf <83098445+csharpdf@users.noreply.github.com>
2021-09-15 23:31:18 -0400

    Make http download zip file

--- main.go
+++ main.go
@@ -1,43 +1,70 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "io"
-  "net/http"
-  "log"
-  "strings"
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
+
 var client = http.Client{}
-func downloadFile(url string) *error {
-  sp := strings.Split(url, "/")
-  err := os.Mkdir(sp[len(sp) - 1], 1)
-  if err != nil {
-    return &err
-  }
-  os.Chdir("./" + sp[len(sp) - 1])
-  var newg string
-  if strings.HasPrefix(url, "github.com") || strings.HasPrefix(url, "https://github.com") {
-    newg = url + "/archives/reps/heads/master.zip"
-  } else {
-    log.Fatal("Unsupported domain given.")
-  }
-  file, err := os.Create(sp[len(sp) - 1] + ".zip")
-  os.Chdir("../")
-  if err != nil {
-    return &err
-  }
-  g, err := client.Get(newg)
-  if err != nil {
-    return &err
-  }
-  io.Copy(file, g.Body)
-  return nil
+
+func downloadFile(url string) error {
+	sp := strings.Split(url, "/")
+	var newg string
+	if strings.HasPrefix(url, "github.com") || strings.HasPrefix(url, "https://github.com") {
+		newg = url + "/archive/refs/heads/master.zip"
+	} else {
+		log.Fatal("Unsupported domain given.")
+	}
+	err := os.Mkdir(sp[len(sp)-1], 1)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(filepath.Join(sp[len(sp)-1], "main.zip"))
+	if err != nil {
+		return err
+	}
+	g, err := client.Get(newg)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(file, g.Body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 func main() {
-  fmt.Println("Attempting to download file...")
-   err := downloadFile(os.Args[1])
-   if err != nil {
-     log.Fatal("Failed to download file/folder:", err)
-   }
+	fmt.Println("Attempting to download file...")
+	err := downloadFile(os.Args[1])
+	if err != nil {
+		log.Fatal("Failed to download file/folder: ", err)
+	}
+	fmt.Println("Successfully downloaded repository zip file!")
+	link := strings.Split(os.Args[1], "/")
+	zip, err := zip.OpenReader(filepath.Join(link[len(link)-1], "/main.zip"))
+	if err != nil {
+		log.Fatal("Failed to unpack file/folder: ", err)
+	}
+	defer zip.Close()
+	for _, fi := range zip.File {
+		fmt.Printf("Unpacking %s..", fi.Name)
+		f, err := os.Create(fi.Name)
+		if err != nil {
+			log.Fatal("Failed to unpack file/folder: ", err)
+		}
+		readerr, err := fi.Open()
+		if err != nil {
+			log.Fatal("Failed to unpack file/folder: ", err)
+		}
+		io.Copy(f, readerr)
+		fmt.Print(" [Done]\n")
+	}
+	fmt.Println("Successfully unzipped files, Exit Code 0")
+	os.Exit(0)
 }
