e79f1a775ad8966d225f9760e3e359032756fb21
csharpdf <83098445+csharpdf@users.noreply.github.com>
2021-09-24 20:19:44 -0400

    Delete pkg.go

--- src/pkg.go
+++
@@ -1,7 +0,0 @@
-//The package commands of gofind may change in the future to create .find files that will automate the build process, instead of creating a package to publish to a database for other users to access.
-package src
- import "fmt"
-
- func main() {
-     
- }
