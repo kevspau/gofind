1520c14dc433c9aca201f07dd2dce2acd391f11d
csharpdf <csharpdfjer@gmail.com>
2021-12-18 13:32:27 -0500

    potential fix args #

--- main.nim
+++ main.nim
@@ -7,36 +7,37 @@ let client = httpclient.newHttpClient()
 if findExe("git") != "":
     if os.paramCount() > 0:
         if os.paramStr(1) == "find":
-            if os.paramCount() > 2 and (os.paramStr(3).startsWith("-b=") or os.paramStr(4).startsWith("-b=")):
-                stdout.styledWriteLine(fgBlue, "Downloading branch ", fgGreen, paramStr(3).split("=")[1], fgBlue, " in repository ", fgGreen, paramStr(2))
-                if client.get(urls[0] & paramStr(2)).status != "404 Not Found":
-                    discard execCmd("git clone " & urls[0] & paramStr(2) & " -b " & paramStr(3).split("=")[1])
-                    stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
-                elif client.get(urls[1] & paramStr(2)).status != "404 Not Found":
-                    discard execCmd("git clone " & urls[1] & paramStr(2) & " -b " & paramStr(3).split("=")[1])
-                    stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
-                elif client.get(urls[2] & paramStr(2)).status != "404 Not Found":
-                    discard execCmd("git clone " & urls[2] & paramStr(2) & " -b " & paramStr(3).split("=")[1])
-                    stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
-                else:
-                    stdout.styledWriteLine(fgRed, "Error: ", resetStyle, "Failed to download repository, either unsupported git website, or repository/branch does not exist.")
-            else: 
-                stdout.styledWriteLine(fgBlue, "Downloading repository ", fgGreen, paramStr(2))
-                if client.get(urls[0] & paramStr(2)).status != "404 Not Found":
-                   # echo client.get(urls[0] & paramStr(2)).status
-                    discard execCmd("git clone " & urls[0] & paramStr(2))
-                    stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
-                elif client.get(urls[1] & paramStr(2)).status != "404 Not Found":
-                    discard execCmd("git clone " & urls[1] & paramStr(2))
-                    stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
-                elif client.get(urls[2] & paramStr(2)).status != "404 Not Found":
-                    discard execCmd("git clone " & urls[2] & paramStr(2))
-                    stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
-                else:
-                    stdout.styledWriteLine(fgRed, "Error: ", resetStyle, "Failed to download repository, either unsupported git website, or repository does not exist.")
-            if os.paramCount() > 2 and (os.paramStr(3) == "-D" or os.paramStr(4) == "-D"):
-                setCurrentDir(getAppDir() & DirSep & paramStr(2).split("/")[1])
-                removeDir(".git") #TODO: Remove other git-related files like .gitignore if found
+            if os.paramCount() == 3 or os.paramCount() == 4: #?Possible fix
+                if os.paramStr(3).startsWith("-b=") or os.paramStr(4).startsWith("-b="): #*has to check paramCount first
+                    stdout.styledWriteLine(fgBlue, "Downloading branch ", fgGreen, paramStr(3).split("=")[1], fgBlue, " in repository ", fgGreen, paramStr(2))
+                    if client.get(urls[0] & paramStr(2)).status != "404 Not Found":
+                        discard execCmd("git clone " & urls[0] & paramStr(2) & " -b " & paramStr(3).split("=")[1])
+                        stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
+                    elif client.get(urls[1] & paramStr(2)).status != "404 Not Found":
+                        discard execCmd("git clone " & urls[1] & paramStr(2) & " -b " & paramStr(3).split("=")[1])
+                        stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
+                    elif client.get(urls[2] & paramStr(2)).status != "404 Not Found":
+                        discard execCmd("git clone " & urls[2] & paramStr(2) & " -b " & paramStr(3).split("=")[1])
+                        stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
+                    else:
+                        stdout.styledWriteLine(fgRed, "Error: ", resetStyle, "Failed to download repository, either unsupported git website, or repository/branch does not exist.")
+                else: 
+                    stdout.styledWriteLine(fgBlue, "Downloading repository ", fgGreen, paramStr(2))
+                    if client.get(urls[0] & paramStr(2)).status != "404 Not Found":
+                    # echo client.get(urls[0] & paramStr(2)).status (debug)
+                        discard execCmd("git clone " & urls[0] & paramStr(2))
+                        stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
+                    elif client.get(urls[1] & paramStr(2)).status != "404 Not Found":
+                        discard execCmd("git clone " & urls[1] & paramStr(2))
+                        stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
+                    elif client.get(urls[2] & paramStr(2)).status != "404 Not Found":
+                        discard execCmd("git clone " & urls[2] & paramStr(2))
+                        stdout.styledWriteLine(fgGreen, "Successfully downloaded repository!")
+                    else:
+                        stdout.styledWriteLine(fgRed, "Error: ", resetStyle, "Failed to download repository, either unsupported git website, or repository does not exist.")
+                if os.paramStr(3) == "-D" or os.paramStr(4) == "-D":
+                    setCurrentDir(getAppDir() & DirSep & paramStr(2).split("/")[1])
+                    removeDir(".git") #TODO: Remove other git-related files like .gitignore if found
         elif os.paramStr(1) == "-h":
             echo "gofind - A small, simple wrapper around git clone.\n\nUsage: gof find [git url] [options]\n\n-h            Display this help text\n-b=<branch name>            Clone a specific branch of a repository\n-D            Delete .git folder after clone\n\n\n" #help text
         else:
